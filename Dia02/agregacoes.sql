53a7aabad72390019e9691f50446561b658d883d
suagomes@gmail.com <suagomes@gmail.com>
2022-12-15 00:10:45 +0000

    a

--- Dia02/agregacoes.sql
+++ Dia02/agregacoes.sql
@@ -1,2 +1,21 @@
 -- Databricks notebook source
+select count (*) as nrLinhasNaoNulas
+from 
+silver_olist.cliente
 
+-- COMMAND ----------
+
+select count (*) as nrLinhasNaoNulas,
+      count (idclientes) As nrIdClienteNaoNulo --id de clientes nao nulos
+from 
+silver_olist.cliente
+
+-- COMMAND ----------
+
+select count (*) as nrLinhasNaoNulas,
+      count (idcliente) As nrIdClienteNaoNulo, --id de clientes nao nulos
+      count (distinct idcliente) As nrIdClienteDistintos, --id de clientes distintos
+      count (distinct idclienteUnico) As nrIdClienteUnico --id de clientes nao nulos
+      
+from 
+silver_olist.cliente
